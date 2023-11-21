00155f5df400f35ac52577ffc1f7d7789e2050e3
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-21 20:46:03 +0300

    Make Calculator instance pass tests

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,3 +1,30 @@
-function Calculator() {}
+function Calculator() {
+  function add(x, y) {
+    checkValidity(x, y);
+    return x + y;
+  }
+
+  function subtract(x, y) {
+    checkValidity(x, y);
+    return x - y;
+  }
+
+  function multiply(x, y) {
+    checkValidity(x, y);
+    return x * y;
+  }
+
+  function divide(x, y) {
+    checkValidity(x, y);
+    return x / y;
+  }
+
+  function checkValidity(x, y) {
+    if (typeof x === "number" && typeof y === "number") return;
+    throw new TypeError("Both arguments must be of type number");
+  }
+
+  return { add, subtract, multiply, divide };
+}
 
 module.exports = Calculator;
