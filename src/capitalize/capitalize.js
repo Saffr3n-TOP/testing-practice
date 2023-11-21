1c1384bb182bf353c10ea98a3122134456d14bec
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-21 20:06:50 +0300

    Make capitalize function pass tests

--- src/capitalize/capitalize.js
+++ src/capitalize/capitalize.js
@@ -1,3 +1,13 @@
-function capitalize(string) {}
+function capitalize(string) {
+  const sentences = string.split(".");
+
+  for (let i = 0; i < sentences.length; i++) {
+    if (!sentences[i]) continue;
+    sentences[i] = sentences[i].trim();
+    sentences[i] = sentences[i][0].toUpperCase() + sentences[i].slice(1);
+  }
+
+  return sentences.join(". ").trim();
+}
 
 module.exports = capitalize;
--- src/capitalize/capitalize.test.js
+++ src/capitalize/capitalize.test.js
@@ -4,24 +4,22 @@ test("capitalizes a simple string", () => {
   expect(capitalize("test")).toBe("Test");
 });
 
-test.skip("capitalizes a multi-sentence string", () => {
-  expect(
-    capitalize("some other string. it has two sentences.").toBe(
-      "Some other string. It has two sentences."
-    )
+test("capitalizes a multi-sentence string", () => {
+  expect(capitalize("some other string. it has two sentences.")).toBe(
+    "Some other string. It has two sentences."
   );
 });
 
-test.skip("trims and capitalizes a string", () => {
+test("trims and capitalizes a string", () => {
   expect(capitalize(" test ")).toBe("Test");
 });
 
-test.skip("throws an error if not a string provided", () => {
-  expect(capitalize(1)).toThrow();
-  expect(capitalize([])).toThrow();
-  expect(capitalize({})).toThrow();
-  expect(capitalize(null)).toThrow();
-  expect(capitalize()).toThrow();
-  expect(capitalize(true)).toThrow();
-  expect(capitalize(() => {})).toThrow();
+test("throws an error if not a string provided", () => {
+  expect(() => capitalize(1)).toThrow();
+  expect(() => capitalize([])).toThrow();
+  expect(() => capitalize({})).toThrow();
+  expect(() => capitalize(null)).toThrow();
+  expect(() => capitalize()).toThrow();
+  expect(() => capitalize(true)).toThrow();
+  expect(() => capitalize(() => {})).toThrow();
 });
