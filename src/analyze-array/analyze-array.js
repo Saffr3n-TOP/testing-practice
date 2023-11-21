e9b2c8300cf83c984946dfc8bbb7854d7b1df0eb
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-21 22:34:19 +0300

    Make analyzeArray function pass tests

--- src/analyze-array/analyze-array.js
+++ src/analyze-array/analyze-array.js
@@ -1,3 +1,24 @@
-function analyzeArray(arr) {}
+function analyzeArray(arr) {
+  if (!Array.isArray(arr)) throw new TypeError();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number") throw new TypeError();
+  }
+
+  const obj = {
+    average: null,
+    min: null,
+    max: null,
+    length: arr.length,
+  };
+
+  if (!arr.length) return obj;
+
+  obj.min = Math.min(...arr);
+  obj.max = Math.max(...arr);
+  obj.average = Math.floor((obj.min + obj.max) / 2);
+
+  return obj;
+}
 
 module.exports = analyzeArray;
